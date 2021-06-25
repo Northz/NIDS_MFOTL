3b6a84ce5c72f3e0eab1ff4d0f5ed2a237ddb3b3
Nico Hauser <me@tyratox.ch>
2021-06-25 14:19:03 +0200

    add new measurements

--- trigger-performance-evaluation/measure-single.sh
+++ trigger-performance-evaluation/measure-single.sh
@@ -4,14 +4,16 @@ cd "./experiments/experiment-$1"
 TIMEFORMAT='%3R;%3U;%3S'
 
 time_rewritten=$(time (../../../monpoly -formula ./formula.txt -no_trigger -log ./log.txt -sig ./signature.txt -no_rw -verified > ./out-rewritten.txt) 2>&1)
-rewritten_loop_time=$(cat ./out-rewritten.txt | grep loop | sed "s/loop: //" | awk '{n += $1}; END{print n}')
+# rewritten_loop_time=$(cat ./out-rewritten.txt | grep loop | sed "s/loop: //" | awk '{n += $1}; END{print n}')
+rewritten_meval_time=$(cat ./out-rewritten.txt | grep meval | sed "s/meval: //" | awk '{n += $1}; END{print n}')
 
 time_native=$(time (../../../monpoly -formula ./formula.txt -log ./log.txt -sig ./signature.txt -no_rw -verified  > ./out-native.txt) 2>&1)
-trigger_time=$(cat ./out-native.txt | grep diff | sed "s/diff: //" | awk '{n += $1}; END{print n}')
-native_loop_time=$(cat ./out-native.txt | grep loop | sed "s/loop: //" | awk '{n += $1}; END{print n}')
+trigger_time=$(cat ./out-native.txt | grep mmtaux | sed "s/mmtaux: //" | awk '{n += $1}; END{print n}')
+# native_loop_time=$(cat ./out-native.txt | grep loop | sed "s/loop: //" | awk '{n += $1}; END{print n}')
+native_meval_time=$(cat ./out-native.txt | grep meval | sed "s/meval: //" | awk '{n += $1}; END{print n}')
 
 # even though we simply discard out-rewritten, it's more comparable this way
 rm ./out-rewritten.txt
 rm ./out-native.txt
 
-echo "$1;$time_rewritten;$rewritten_loop_time;$time_native;$native_loop_time;$trigger_time"
+echo "$1;$time_rewritten;$rewritten_meval_time;$time_native;$trigger_time;$native_meval_time"
--- trigger-performance-evaluation/plot-generator.py
+++ trigger-performance-evaluation/plot-generator.py
@@ -25,18 +25,24 @@ df = pd.read_csv(measurements, sep=";", quotechar='"', skipinitialspace=True)
 #         'rewritten sys time', 'native sys time'], axis=1)
 
 df = df.sort_values(by=['experiment']).groupby('experiment').agg({
-    'rewritten loop time': ['mean', 'std'],
-    'native loop time': ['mean', 'std'],
+    'rewritten real time': ['mean', 'std'],
+    'rewritten meval time': ['mean', 'std'],
+    'native real time': ['mean', 'std'],
+    'native meval time': ['mean', 'std'],
     'native trigger time': ['mean', 'std']
 })
 
 labels = ["experiment " + str(i) for i in df.index.to_numpy()]
-rewritten_means = df['rewritten loop time']['mean'].to_numpy()
-native_means = df['native loop time']['mean'].to_numpy()
+#rewritten_means = df['rewritten real time']['mean'].to_numpy()
+rewritten_meval_means = df['rewritten meval time']['mean'].to_numpy()
+#native_means = df['native real time']['mean'].to_numpy()
+native_meval_means = df['native meval time']['mean'].to_numpy()
 native_mmtaux_means = df['native trigger time']['mean'].to_numpy()
 
-rewritten_stds = df['rewritten loop time']['std'].to_numpy()
-native_stds = df['native loop time']['std'].to_numpy()
+#rewritten_stds = df['rewritten real time']['std'].to_numpy()
+rewritten_meval_stds = df['rewritten meval time']['std'].to_numpy()
+#native_stds = df['native real time']['std'].to_numpy()
+native_meval_stds = df['native meval time']['std'].to_numpy()
 native_mmtaux_stds = df['native trigger time']['std'].to_numpy()
 
 plt.rcParams["figure.figsize"] = (len(labels)+3, 5)
@@ -48,16 +54,20 @@ x = np.arange(len(labels))
 width = 0.35
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(x - width/2, rewritten_means, width,
-                label='rewritten', yerr=rewritten_stds, ecolor='black', capsize=2)
-rects2 = ax.bar(x + width/2, native_means, width,
-                label='native', yerr=native_stds, ecolor='black', capsize=2)
-# render them on top of rects2
-rects3 = ax.bar(x + width/2, native_mmtaux_stds, width,
-                label='mmtaux', yerr=native_mmtaux_stds, ecolor='black', capsize=2)
+# rects_rewritten = ax.bar(x - width/2, rewritten_means, width,
+#                          label='rewritten', yerr=rewritten_stds, ecolor='black', capsize=2, color='#3498db')
+rects_rewritten_meval = ax.bar(x - width/2, rewritten_meval_means, width,
+                               label='rewritten meval', yerr=rewritten_meval_stds, ecolor='black', capsize=2)  # , color='#2980b9'
+
+# rects_native = ax.bar(x + width/2, native_means, width,
+#                       label='native', yerr=native_stds, ecolor='black', capsize=2, color='#e67e22')
+rects_native_meval = ax.bar(x + width/2, native_meval_means, width,
+                            label='native meval', yerr=native_meval_stds, ecolor='black', capsize=2)  # , color='#d35400'
+rects_native_mmtaux = ax.bar(x + width/2, native_mmtaux_means, width,
+                             label='mmtaux', yerr=native_mmtaux_stds, ecolor='black', capsize=2)  # , color='#2ecc71'
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Running time in user mode in seconds')
+ax.set_ylabel('Running time of meval in seconds')
 ax.set_title(title)
 ax.set_xticks(x)
 ax.set_xticklabels(labels)
